32e7751dcd0874d3e7a8637ce45b73fd7841b178
Chris Margroff <sora3087@gmail.com>
2025-05-09 18:59:11 -0400

    add animated elements to scene

--- scripts/views/Game.cs
+++ scripts/views/Game.cs
@@ -3,9 +3,16 @@ using JamTemplate.Util;
 
 public partial class Game : Node3D
 {
+  private AnimationPlayer _animationPlayer;
   [FromServices]
   public void Inject()
   {
 
   }
+  public override void _EnterTree()
+  {
+    _animationPlayer = GetNode<AnimationPlayer>("%AnimationPlayer");
+    _animationPlayer.CurrentAnimation = "rocking";
+  }
+
 }
