fcfcfa27430a7e97a5a0d5ff0d8845ddafd30156
Chris Margroff <sora3087@gmail.com>
2025-05-09 00:01:07 -0400

    sm64 deterministic RandomU16

--- scripts/services/RandomNumberGeneratorService.cs
+++ scripts/services/RandomNumberGeneratorService.cs
@@ -4,6 +4,7 @@ namespace JamTemplate.Services;
 
 public partial class RandomNumberGeneratorService
 {
+  static ushort gRandomSeed16;
   private RandomNumberGenerator _rng;
   public RandomNumberGeneratorService()
   {
@@ -21,5 +22,40 @@ public partial class RandomNumberGeneratorService
   public uint GetInt()
   {
     return _rng.Randi();
+  }
+  public ushort RandomU16()
+  {
+    ushort temp1, temp2;
+
+    if (gRandomSeed16 == 22026)
+    {
+      gRandomSeed16 = 0;
+    }
+
+    temp1 = (ushort)((gRandomSeed16 & 0x00FF) << 8);
+    temp1 = (ushort)(temp1 ^ gRandomSeed16);
+
+    gRandomSeed16 = (ushort)(((temp1 & 0x00FF) << 8) + ((temp1 & 0xFF00) >> 8));
+
+    temp1 = (ushort)(((temp1 & 0x00FF) << 1) ^ gRandomSeed16);
+    temp2 = (ushort)((temp1 >> 1) ^ 0xFF80);
+
+    if ((temp1 & 1) == 0)
+    {
+      if (temp2 == 43605)
+      {
+        gRandomSeed16 = 0;
+      }
+      else
+      {
+        gRandomSeed16 = (ushort)(temp2 ^ 0x1FF4);
+      }
+    }
+    else
+    {
+      gRandomSeed16 = (ushort)(temp2 ^ 0x8180);
+    }
+
+    return gRandomSeed16;
   }
 }
