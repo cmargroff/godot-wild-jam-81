a02c79c88e4dea90fd6fa551db3cb4a82b33cea7
Chris Margroff <sora3087@gmail.com>
2025-05-10 14:31:58 -0400

    add ship component script

---
+++ scripts/components/game/Ship.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+public partial class Ship : Node3D
+{
+  private AnimationPlayer _animationPlayer;
+  private MeshInstance3D _model;
+  public override void _EnterTree()
+  {
+    _model = GetNode<MeshInstance3D>("Model");
+    _animationPlayer = GetNode<AnimationPlayer>("%AnimationPlayer");
+    _animationPlayer.CurrentAnimation = "rocking";
+  }
+}
