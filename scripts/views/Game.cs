564bd52d2dabb1a978fd8b0463aead4cf8ae0b7b
Chris Margroff <sora3087@gmail.com>
2025-05-10 00:38:11 -0400

    add game event manager to game dependencies

--- scripts/views/Game.cs
+++ scripts/views/Game.cs
@@ -6,15 +6,21 @@ public partial class Game : Node3D
 {
   private AnimationPlayer _animationPlayer;
   private SceneManager _sceneManager;
+  private GameEventManager _eventManager;
   [FromServices]
-  public void Inject(SceneManager sceneManager)
+  public void Inject(SceneManager sceneManager, GameEventManager eventManager)
   {
     _sceneManager = sceneManager;
+    _eventManager = eventManager;
+    AddChild(_eventManager);
   }
   public override void _EnterTree()
   {
     _animationPlayer = GetNode<AnimationPlayer>("%AnimationPlayer");
     _animationPlayer.CurrentAnimation = "rocking";
   }
-
+  public override void _Ready()
+  {
+    _eventManager.Start();
+  }
 }
