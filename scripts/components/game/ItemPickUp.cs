c266badfdbdbdcb325a25bfd2fb2f17719f3f40b
Chris Margroff <sora3087@gmail.com>
2025-05-11 11:38:37 -0400

    mouse hover events for items

--- scripts/components/game/ItemPickUp.cs
+++ scripts/components/game/ItemPickUp.cs
@@ -3,21 +3,35 @@ using Godot;
 public partial class ItemPickUp : Node3D
 {
     [Export]
-    int SPEED = 5;
+    float SPEED = 5f;
     private Vector3 _position;
+    private bool _hovered = false;
+
     public override void _Ready()
     {
-        _position = GlobalPosition;
+        _position = Position;
+        var area = GetNode<Area3D>("Area3D");
+        area.Connect(Area3D.SignalName.MouseEntered, Callable.From(MouseEntered));
+        area.Connect(Area3D.SignalName.MouseExited, Callable.From(MouseExited));
     }
 
     public override void _PhysicsProcess(double delta)
     {
         _position.X -= SPEED * (float)delta;
-        GlobalPosition = _position;
-        if (GlobalPosition.X <= -20.0f)
+        Position = _position;
+        if (Position.X <= -20.0f)
         {
             QueueFree();
         }
     }
-
+    public void MouseEntered()
+    {
+        GD.Print("mouseEntered");
+        _hovered = true;
+    }
+    public void MouseExited()
+    {
+        GD.Print("mouseExited");
+        _hovered = false;
+    }
 }
