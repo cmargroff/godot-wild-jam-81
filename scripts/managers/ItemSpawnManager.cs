dc1bb1ebb8db44abebaaacfb6651026c3948ef28
Chris Margroff <sora3087@gmail.com>
2025-05-10 22:13:53 -0400

    item spawn manager store reference ot packed scene

--- scripts/managers/ItemSpawnManager.cs
+++ scripts/managers/ItemSpawnManager.cs
@@ -1,17 +1,17 @@
 using Godot;
-using System;
 
 public partial class ItemSpawnManager : Node
 {
     private Vector3 _position = new Vector3(20, 0, -6);
-    public void Spawn()
+    private PackedScene _itemScene;
+    public override void _EnterTree()
     {
-        var itemScene = GD.Load<PackedScene>("res://components/game/ItemPickUp.tscn");
-        Node3D itemSceneInstance = itemScene.Instantiate<Node3D>();
+        _itemScene = GD.Load<PackedScene>("res://components/game/ItemPickUp.tscn");
+    }
+    public void Spawn(string identifier)
+    {
+        Node3D itemSceneInstance = _itemScene.Instantiate<Node3D>();
         itemSceneInstance.Position = _position;
         GetTree().CurrentScene.AddChild(itemSceneInstance);
-
     }
-
-
 }
