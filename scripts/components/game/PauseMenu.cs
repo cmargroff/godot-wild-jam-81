724f22f7a983209c9990498a9e17cd08155bd216
hakalochka <hakalochka@gmail.com>
2025-05-16 14:59:55 +0200

    added menu button

--- scripts/components/game/PauseMenu.cs
+++ scripts/components/game/PauseMenu.cs
@@ -7,23 +7,33 @@ using System;
 public partial class PauseMenu : Control
 {
     private Button _continue;
+    private Button _menu;
     private PauseManager _pauseManager;
+    private SceneManager _sceneManager;
 
     public override void _Ready()
     {
         Visible = false;
         _continue = GetNode<Button>("%Continue");
         _continue.Pressed += Continue;
+        _menu = GetNode<Button>("%Menu");
+        _menu.Pressed += Menu;
         _pauseManager = Globals.ServiceProvider.GetRequiredService<PauseManager>();
         GD.Print(_pauseManager);
         _pauseManager.GamePauseChanged += PauseManager_GamePauseChanged;
-        
+        _sceneManager = Globals.ServiceProvider.GetRequiredService<SceneManager>();
 
     }
 
     private void Continue()
     {
         _pauseManager.Unpause();
+    }
+
+    private void Menu()
+    {
+        _pauseManager.Unpause();
+        _sceneManager.ChangeScene("Title");
     }
 
     public void PauseManager_GamePauseChanged(bool paused)
