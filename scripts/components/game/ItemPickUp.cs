9621647b5595a34f3da6fa0bf188f1a99607bd4f
Chris Margroff <sora3087@gmail.com>
2025-05-18 11:42:43 -0400

    add item buoyancy [wip]

--- scripts/components/game/ItemPickUp.cs
+++ scripts/components/game/ItemPickUp.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 using Microsoft.Extensions.DependencyInjection;
 using ShipOfTheseus2025.Enum;
@@ -12,13 +13,21 @@ public partial class ItemPickUp : Node3D
     [Export]
     float VELOCITY = 2f;
 
-    private Vector3 _position;
+    private Vector3 _globalPosition;
     private bool _hovered = false;
     private ItemDragManager _dragManager;
     private HoverPanelManager _hoverManager;
     private Area3D _area;
     public InventoryItem InventoryItem { get; set; }
     public ItemPickupState State;
+
+    private float _time = 0;
+    private float noise_scale;
+    private Vector2 noise_speed;
+    private float noise_strength;
+    private Image noise;
+
+    private List<Node3D> _waterSamplers = new();
 
     public enum ItemPickupState
     {
@@ -35,12 +44,45 @@ public partial class ItemPickUp : Node3D
         _dragManager = Globals.ServiceProvider.GetRequiredService<ItemDragManager>();
         _dragManager.PickupAudioStreamPlayer = ItemPickupAudioPlayer;
         _hoverManager = Globals.ServiceProvider.GetRequiredService<HoverPanelManager>();
-        _position = Position;
+        _globalPosition = GlobalPosition;
         var area = GetNode<Area3D>("Area3D");
         _area = area;
         area.Connect(Area3D.SignalName.MouseEntered, Callable.From(MouseEntered));
         area.Connect(Area3D.SignalName.MouseExited, Callable.From(MouseExited));
         AddChild(InventoryItem.ItemScene);
+
+        // create references to water noise
+        var water = GetNode<MeshInstance3D>("/root/SceneManager/Game/Water");
+        var material = water.Mesh.SurfaceGetMaterial(0) as ShaderMaterial;
+        noise = material.GetShaderParameter("noise1").As<NoiseTexture2D>().Noise.GetSeamlessImage(512, 512, false, false, 0.1f, true);
+        noise_scale = (float)material.GetShaderParameter("noise1_scale");
+        noise_speed = (Vector2)material.GetShaderParameter("noise1_speed");
+        noise_strength = (float)material.GetShaderParameter("noise1_strength");
+        _time = (float)material.GetShaderParameter("wave_time");
+
+        var samplerParent = InventoryItem.ItemScene.GetNode("WaterSamplers");
+        if (samplerParent is null)
+        {
+            // use root node as sampler
+            _waterSamplers = new List<Node3D> { InventoryItem.ItemScene };
+        }
+        else
+        {
+            // get all children of the sampler parent
+            for (int i = 0; i < samplerParent.GetChildCount(); i++)
+            {
+                var child = samplerParent.GetChild(i);
+                if (child is Node3D node)
+                {
+                    _waterSamplers.Add(node);
+                }
+            }
+            // limit list to 2
+            if (_waterSamplers.Count > 2)
+            {
+                _waterSamplers = _waterSamplers.GetRange(0, 2);
+            }
+        }
     }
 
     public override void _PhysicsProcess(double delta)
@@ -48,16 +90,18 @@ public partial class ItemPickUp : Node3D
 
         if (State == ItemPickupState.Floating)
         {
-            _position.X -= SPEED * (float)delta;
-            Position = _position;
+            _globalPosition.X -= SPEED * (float)delta;
+            GlobalPosition = _globalPosition;
+
+            UpdateBuoyancy(delta);
         }
 
         if (State == ItemPickupState.Dropped)
         {
-            _position.Y -= VELOCITY * (float)delta;
+            _globalPosition.Y -= VELOCITY * (float)delta;
             //maybe
             // _position.X -= (SPEED/2) * (float)delta;
-            Position = _position;
+            GlobalPosition = _globalPosition;
             VELOCITY += 0.1f;
 
         }
@@ -81,7 +125,7 @@ public partial class ItemPickUp : Node3D
                 GD.Print("hover page");
 
             }
-          
+
 
         }
         if (@event.IsPressed() && @event.IsAction("rmb"))
@@ -92,7 +136,7 @@ public partial class ItemPickUp : Node3D
                 MouseExited();
                 State = ItemPickupState.Dropped;
                 GD.Print(GlobalPosition);
-                _position = GlobalPosition;
+                _globalPosition = GlobalPosition;
                 _hoverManager.HidePage();
 
             }
@@ -125,12 +169,45 @@ public partial class ItemPickUp : Node3D
             _hovered = (State == ItemPickupState.Floating) ? true : false;
             if (_hovered) _hoverManager.ShowItem(InventoryItem, HoverType.Item);
         }
-        
+
     }
     public void MouseExited()
     {
         _hovered = false;
         if (_dragManager.Dragging == false && State == ItemPickupState.Floating) _hoverManager.HidePage();
         GD.Print("mouse exited");
+    }
+    private void UpdateBuoyancy(double delta)
+    {
+        _time += (float)delta;
+        if (_waterSamplers.Count == 0) return;
+        if (_waterSamplers.Count == 1)
+        {
+            var height = GetHeight(_waterSamplers[0].GlobalPosition);
+            GlobalPosition = new Vector3(GlobalPosition.X, height + 2.25f, GlobalPosition.Z);
+        }
+        else
+        {
+            var height = GetHeight(_waterSamplers[0].GlobalPosition);
+            var height2 = GetHeight(_waterSamplers[1].GlobalPosition);
+            var heightdiff = height2 - height;
+            Vector2 slopevect = new Vector2(1, heightdiff).Normalized();
+            float newrotx = new Vector2(1, 0).AngleTo(slopevect);
+            newrotx = Mathf.Clamp(newrotx, -0.35f, 0.35f);
+            Rotation = new Vector3(newrotx, Rotation.Y, Rotation.Z);
+        }
+    }
+    private float GetHeight(Vector3 position)
+    {
+        var uv_x = Mathf.Wrap(position.X / noise_scale + _time * noise_speed.X, 0, 1);
+        var uv_y = Mathf.Wrap(position.Z / noise_scale + _time * noise_speed.Y, 0, 1);
+
+
+        var pixel_pos = new Vector2I(
+            Mathf.RoundToInt(uv_x * (noise.GetWidth() - 1)),
+            Mathf.RoundToInt(uv_y * (noise.GetHeight() - 1))
+        );
+        GD.Print((uv_x, pixel_pos.X));
+        return noise.GetPixelv(pixel_pos).R * noise_strength;
     }
 }
