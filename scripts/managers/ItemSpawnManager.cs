04bcf3ec58efbca3a857ad716bf9ff89951dc9bd
Chris Margroff <sora3087@gmail.com>
2026-06-23 16:11:38 -0400

    use audio manager for item spawning

--- scripts/managers/ItemSpawnManager.cs
+++ scripts/managers/ItemSpawnManager.cs
@@ -5,37 +5,47 @@ using ShipOfTheseus2025.Resources;
 
 public partial class ItemSpawnManager : Node, IItemSpawnManager
 {
+    [Export]
+    public PackedScene ItemScene;
+    [Export]
+    public AudioStream ItemPickupAudio;
     private Vector3 _position = new Vector3(40, 4, 11);
-    private PackedScene _itemScene;
     private ISceneManager _sceneManager;
     private ItemFactoryService _itemFactoryService;
-    public AudioStreamPlayer3D ItemPickupAudio { get; private set; }
+    private IAudioManager _audioManager;
+    private AudioStreamPlayer _pickupSFX;
 
     [FromServices]
-    public void Inject(ISceneManager sceneManager, ItemFactoryService itemFactoryService)
+    public void Inject(ISceneManager sceneManager, ItemFactoryService itemFactoryService, IAudioManager audioManager)
     {
         _sceneManager = sceneManager;
         _itemFactoryService = itemFactoryService;
+        _audioManager = audioManager;
     }
 
     public override void _EnterTree()
     {
         Name = GetType().Name;
-        _itemScene = GD.Load<PackedScene>("res://components/game/ItemPickUp.tscn");
     }
 
     public override void _Ready()
     {
-        // TODO get this from globals
-        ItemPickupAudio = GetParent().GetParent().GetNode<AudioStreamPlayer3D>("%AudioStreamPlayer_ItemPickup");
+        _pickupSFX = new AudioStreamPlayer();
+        _pickupSFX.Stream = ItemPickupAudio;
+        AddChild(_pickupSFX);
     }
 
     public void Spawn(string identifier)
     {
+        if (ItemScene == null)
+        {
+            GD.PrintErr("ItemScene is not assigned in the inspector.");
+            return;
+        }
         ItemResource resource = _sceneManager.PreloadedResources["Items"][identifier] as ItemResource;
         InventoryItem item = _itemFactoryService.GenerateItem(resource);
-        ItemPickUp pickupableItem = _itemScene.Instantiate<ItemPickUp>();
-        pickupableItem.ItemPickupAudioPlayer = ItemPickupAudio;
+        ItemPickUp pickupableItem = ItemScene.Instantiate<ItemPickUp>();
+        pickupableItem.ItemPickupAudioPlayer = _pickupSFX;
         pickupableItem.InventoryItem = item;
         pickupableItem.Position = _position;
         GetTree().CurrentScene.AddChild(pickupableItem);
