30569d14e1095dcc5d5e2e66755a32b3411fc8df
Chris Margroff <sora3087@gmail.com>
2026-06-29 19:36:41 -0400

    make an item script for visual element and add circular reference to inventory item

--- scripts/components/game/InventoryItem.cs
+++ scripts/components/game/InventoryItem.cs
@@ -21,5 +21,5 @@ public class InventoryItem
 
     public Texture2D IconTexture { get; set; }
 
-    public Node3D ItemScene { get; set; }
+    public Node3D Node { get; set; }
 }
---
+++ scripts/components/game/Item.cs
@@ -0,0 +1,21 @@
+using Godot;
+using ShipOfTheseus2025.Components.Game;
+
+public partial class Item : Node3D
+{
+  public InventoryItem InventoryItem { get; set; }
+  public IWaterMover WaterMover { get; set; }
+  public IDragHandler DragHandler { get; set; }
+  public IHoverHandler HoverHandler { get; set; }
+  public Area3D Area { get; private set; }
+  public override void _EnterTree()
+  {
+    var area = GetNode<Area3D>("Display/%Area3D");
+    if (area == null)
+    {
+      GD.PrintErr("Area3D node not found in Item");
+      QueueFree();
+    }
+    Area = area;
+  }
+}
